1b46dfc533cda1cc76c0ae057cd201d464248fad
Irf-z-sam <irfansamsi555@gmail.com>
2024-01-31 21:06:36 +0530

    Creating-Login-Screen-Frontend-and-Make-the-backend-api-of-the-login(Task-3)

--- backendcode/app.js
+++ backendcode/app.js
@@ -3,17 +3,21 @@ const cors = require('cors');
 const bodyparser = require('body-parser');
 const sequelize = require('./util/database');
 const expenseDetails = require('./routes/addexpense');
+const signupDetails = require('./routes/signuppage');
+const loginDetails = require('./routes/loginpage');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyparser.json());
-app.use(expenseDetails)
+app.use(expenseDetails);
+app.use(signupDetails);
+app.use(loginDetails);
 
 sequelize.sync()
 .then(()=>{
-    app.listen(3000,()=>{
-        console.log('server running on 3000 port');
+    app.listen(700,()=>{
+        console.log('server running on 700 port');
     })
 })
 .catch((error)=>{
